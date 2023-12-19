c8c22a83ddd6abcabcae6314e810bc4b24eb8f33
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2023-12-19 09:12:59 +0200

    minor

--- soml-simplify/soml-simplify.pl
+++ soml-simplify/soml-simplify.pl
@@ -1,8 +1,14 @@
-#!/usr/bin/perl
-
+#!perl
 use strict;
 use warnings;
 use YAML::PP;
+
+sub graphql_name($) {
+  # replace punctuation in RDF classes and props (`:.`) with a GraphQL char (`_`)
+  my $name = shift;
+  $name =~ tr{:.}{_};
+  $name
+}
 
 # Load YAML file
 my $yaml = YAML::PP->new (indent => 2, header => 0);
@@ -15,13 +21,13 @@ my $simplified = {};
 for my $object (keys %$objects) {
   my $props    = $objects->{$object}{props} // {};
   my $inherits = $objects->{$object}{inherits};
-  $simplified->{$object}{ISA} = $inherits if $inherits;
+  $simplified->{graphql_name($object)}{ISA} = graphql_name($inherits) if $inherits;
   for my $p (keys %$props) {
-    my $prop     = $props->{$p} // {};        # global prop definition
+    my $prop     = $props->{$p} // {};        # per-object prop definition
     my $property = $properties->{$p} // {};   # global prop definition
-    my $range    = $prop->{range} // $property->{range} // "string";
+    my $range    = graphql_name($prop->{range} // $property->{range} // "string");
     my $max      = $prop->{max} // $property->{max} // 1;
-    $simplified->{$object}{$p} = $max eq 'inf' || $max > 1 ? "[$range]" : $range;
+    $simplified->{graphql_name($object)}{graphql_name($p)} = $max eq 'inf' || $max > 1 ? "[$range]" : $range;
   }
 }
 
